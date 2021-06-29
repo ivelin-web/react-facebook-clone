e280b39e60def1fef24f260114bc1b1414685649
ivelin-web <72993041+ivelin-web@users.noreply.github.com>
2021-06-29 03:07:20 +0300

    Added environment file and hide firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,7 @@
 import firebase from "firebase";
+import environment from "../environment";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAiQljitlNrctDXqw8idmOCP0SXAuot3QI",
-    authDomain: "fb-clone-react-30363.firebaseapp.com",
-    projectId: "fb-clone-react-30363",
-    storageBucket: "fb-clone-react-30363.appspot.com",
-    messagingSenderId: "700178651196",
-    appId: "1:700178651196:web:8c68c2450abeb41f8dcb39",
-    measurementId: "G-E1EGJ2ZQ9Y",
-};
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(environment.firebaseConfig);
 const db = firebaseApp.firestore();
 const storage = firebase.storage();
 const auth = firebase.auth();
